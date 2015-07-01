4b8e53fbea2e3126ad5c2a118f87f6a3b53e642e
Daniel Tschan <tschan@puzzle.ch>
2015-07-01 12:08:40 +0200

    Added Vagrant and Puppet configuration for Beta 4.

--- beta4/login.sh
+++ beta4/login.sh
@@ -4,10 +4,10 @@
 #touch /home/joe/.kube/.kubeconfig
 #chown -R joe.joe /home/joe/.kube
 
-su - joe -c 'osc login -u joe \
---certificate-authority=/var/lib/openshift/openshift.local.certificates/ca/cert.crt \
+echo redhat | su - joe -c 'osc login -u joe \
+--certificate-authority=/etc/openshift/master/ca.crt \
 --server=https://ose3-master.example.com:8443'
 
-su - alice -c 'osc login -u alice \
---certificate-authority=/var/lib/openshift/openshift.local.certificates/ca/cert.crt \
+echo redhat | su - alice -c 'osc login -u alice \
+--certificate-authority=/etc/openshift/master/ca.crt \
 --server=https://ose3-master.example.com:8443'
